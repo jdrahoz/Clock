ecca1d4ee3b804513dbcac90f30fefed0281885b
Julia Drahozal <jdrahoz@ku.edu>
2016-02-02 15:01:44 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Feb 2, 2016
+ *      Author: Julia Drahozal
+ */
+
+#include <iostream>
+using namespace std;
+#include "menu.h"
+
+int main () {
+
+	Menu* menu = new Menu ();
+
+	delete menu;
+
+    return 0;
+};
+
