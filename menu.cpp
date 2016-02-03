9041691ffe23bf3b62059361af3d7308be1de6c4
Julia Drahozal <jdrahoz@ku.edu>
2016-02-02 18:23:52 -0600

    Add new methods and variables

--- menu.cpp
+++ menu.cpp
@@ -2,7 +2,7 @@
  * menu.cpp
  *
  *  Created on: Feb 2, 2016
- *      Author: Julia Drahozal
+ *      Author: Julia Drahozal and Kate Strombom
  */
 
 #include <iostream>
@@ -12,33 +12,159 @@ using namespace std;
 // constructor
 Menu::Menu ()
 {
+    
+    cout<< "welcome to clock\n";
+    callMenu();
+    
+}
 
-	cout<< "welcome to clock\n";
-	cout<< "set current time: ";
-	cin>> currTime;
-	cout<< "set mode: ";
-	cin>> currMode;
-
-};
-
-void Menu::resetTime (int newTime)
+void Menu::callMenu ()
 {
-	currTime = newTime;
-	return;
+    
+    changeMode ();
+    resetTime (currTime);
+    
+    
+}
 
-};
-
-void Menu::changeMode(int newMode)
+void Menu::resetTime (int oldTime)
 {
+    
+    cout<< "set current time: ";
+    
+    if (currMode == 1)
+    {
+        while (1)
+        {
+            cout<< "\nenter hour: ";
+            cin>> hour;
+            if (hour >= 0 and hour <= 23)
+            {
+                break;
+            } else
+            {
+                cout << "\nenter hour between 0 and 23";
+            }
+            
+        }
+        
+        while (1)
+        {
+            cout<< "\nenter minute: ";
+            cin>> minute;
+            if (minute >= 0 and minute <= 59)
+            {
+                break;
+            } else
+            {
+                cout << "\nenter minute between 0 and 59";
+            }
+            
+        }
+        
+        while (1)
+        {
+            cout<< "\nenter second: ";
+            cin>> second;
+            if (second >= 0 and second <= 59)
+            {
+                break;
+            } else
+            {
+                cout << "\nenter second between 0 and 59";
+            }
+            
+        }
+        
+    }
+    
+    if (currMode == 2)
+    {
+        while (1)
+        {
+            cout<< "\nenter hour: ";
+            cin>> hour;
+            if (hour >= 1 and hour <= 12)
+            {
+                break;
+            } else
+            {
+                cout << "\nenter hour between 1 and 12";
+            }
+            
+        }
+        
+        while (1)
+        {
+            cout<< "\nenter minute: ";
+            cin>> minute;
+            if (minute >= 0 and minute <= 59)
+            {
+                break;
+            } else
+            {
+                cout << "\nenter minute between 0 and 59";
+            }
+            
+        }
+        
+        while (1)
+        {
+            cout<< "\nenter second: ";
+            cin>> second;
+            if (second >= 0 and second <= 59)
+            {
+                break;
+            } else
+            {
+                cout << "\nenter second between 0 and 59";
+            }
+            
+        }
+        
+        while (1)
+        {
+            
+            cout << "\nchoose mode: \n  (1) am \n  (2) pm \n";
+            cin >> ampm;
+            
+            if (ampm == 1 || ampm ==2)
+            {
+                break;
+            } else
+            {
+                cout << "\n choose 1 or 2";
+            }
+        }
+        
+    }
+    
+}
 
-};
+void Menu::changeMode()
+{
+    
+    while (1)
+    {
+        
+        cout << "choose mode: \n  (1) 24 hour \n  (2) 12 hour \n";
+        cin >> currMode;
+        
+        if (currMode == 1 || currMode ==2)
+        {
+            break;
+        } else
+        {
+            cout << "\nchoose 1 or 2";
+        }
+    }
+    
+}
 
 
 // destructor
 Menu::~Menu ()
 {
-
-
-};
+}
 
 
