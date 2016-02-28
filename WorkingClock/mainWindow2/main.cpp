e3b549a6b0422d2249f4e6328392c326b1ad0f26
Austin Bailey <a604b997@ku.edu>
2016-02-28 13:37:48 -0600

    Add Calendar Class that determines day of week based on month date combonation for 2016

---
+++ WorkingClock/mainWindow2/main.cpp
@@ -0,0 +1,11 @@
+#include "mainwindow2.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow2 w;
+    w.show();
+
+    return a.exec();
+}
